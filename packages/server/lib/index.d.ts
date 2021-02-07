cfe3cbb95d87590bd7c501b245eb3aa6011f2d5c
lynnkoo <lynnkoo.1120@gmail.com>
2021-02-07 17:41:45 +0800

    update for version

--- packages/server/lib/index.d.ts
+++ packages/server/lib/index.d.ts
@@ -1,4 +1,4 @@
 export * from './middlewares';
-export * from './modules/conf';
+export * from './modules/server';
 export * from './modules/aliyun';
 export * from './routers/ssrRouter';
--- packages/server/lib/index.js
+++ packages/server/lib/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 tslib_1.__exportStar(require("./middlewares"), exports);
-tslib_1.__exportStar(require("./modules/conf"), exports);
+tslib_1.__exportStar(require("./modules/server"), exports);
 tslib_1.__exportStar(require("./modules/aliyun"), exports);
 tslib_1.__exportStar(require("./routers/ssrRouter"), exports);
